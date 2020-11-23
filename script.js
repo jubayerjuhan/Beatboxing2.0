7064ed2825804af742007a54f2bfb2862577f517
jubayerjuhan <davidjuhan23@gmail.com>
2020-11-23 19:32:52 +0600

    Added Lights In Every Button

--- script.js
+++ script.js
@@ -33,27 +33,35 @@ let beats = {
     },
     "83": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
+        button: new Button ('#00fffe', 83)
     },
     "68": {
         beat: new Beat("./assets/Piano Chord 208.mp3"),
+        button: new Button ('#00fffe', 68)
     },
     "70": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button ('#00fffe', 70)
     },
     "71": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button ('#00fffe', 71)
     },
     "72": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button ('#00fffe', 72)
     },
     "74": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button ('#00fffe', 74)
     },
     "75": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button ('#00fffe', 75)
     },
     "76": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button ('#00fffe', 76)
     }
 }
 
@@ -69,7 +77,7 @@ triggerBeat = (event) => {
         console.log("It's Working");
         let keyPress = beats[keyCode];
         keyPress.beat.audioPlayer();
-        keyPress.beat.select()
+        keyPress.button.select()
     }
 }
 
